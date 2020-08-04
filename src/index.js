21c932469182f090b533417f5ed8fafe1d2a9ec2
barbaracfonseca <babi.fonseca@gmail.com>
2020-08-04 14:39:38 +0100

    altered routes to page error

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
 
-import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import RegistrationVideo from './pages/registration/video';
-import RegistrationCategory from './pages/registration/Category'; 
+import RegistrationCategory from './pages/registration/Category';
 
-const Page404 = () => (<div>Page 404</div>)
-//can i use like this.
+const Page404 = () => (<div>Page 404</div>);
+// can i use like this.
 
 ReactDOM.render(
-     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/registration/video" component={RegistrationVideo} />
-        <Route path="/registration/Category" component={RegistrationCategory} />
-        <Route component={Page404} />
-      </Switch>
-     </BrowserRouter>,   
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/registration/video" component={RegistrationVideo} />
+      <Route path="/registration/Category" component={RegistrationCategory} />
+      <Route component={Page404} />
+    </Switch>
+  </BrowserRouter>,
 
-  //<React.StrictMode>
-   // <App />
-  //</React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  // <App />
+  // </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-
